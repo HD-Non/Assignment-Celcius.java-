b96708f635832f9f5c9d2741abef142d8856e645
HD-Non <naruapon@gmail.com>
2024-08-06 12:34:48 +0700

    first commitSC

---
+++ celsius.java
@@ -0,0 +1,8 @@
+public class celsius {
+    public static void main(String[] args) {
+        double fahrenheit = 80.0;
+        double celsius = (5/9.0)*(fahrenheit-32);
+        System.out.println("fahrenheit="+ fahrenheit);
+        System.out.println("celsius="+ celsius);
+    }
+}
